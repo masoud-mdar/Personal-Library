8a05937120f2472fd3501394ae27e2b13b669862
Masoud <masoud_mdar@yahoo.fr>
2021-09-03 10:48:35 +0200

    deploy configs

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,6 +1,2 @@
-//export const BASE_URL = "https://personal-library-msd.herokuapp.com"
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = "https://personal-library-msd.herokuapp.com"
 export const user = "demoUser"
-
-
-// to change BASE_URL and to delete DB in .env
