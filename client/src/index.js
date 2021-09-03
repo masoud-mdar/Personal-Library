51dbfeff670908848bbde4fd75e68309c1914516
Masoud <masoud_mdar@yahoo.fr>
2021-09-03 10:45:55 +0200

    responsive

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./styles.css"
+
 import App from "./components/App"
+
+import "./styles/navbar.css"
+import "./styles/mainPart.css"
+import "./styles/bookInfo.css"
+import "./styles/comments.css"
+import "./styles/styles.css"
 
 ReactDOM.render(<App />, document.getElementById("root"))
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,6 @@
-export const BASE_URL = "https://personal-library-msd.herokuapp.com"
+//export const BASE_URL = "https://personal-library-msd.herokuapp.com"
+export const BASE_URL = "http://localhost:5000"
 export const user = "demoUser"
+
+
+// to change BASE_URL and to delete DB in .env
