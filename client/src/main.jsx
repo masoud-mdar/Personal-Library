300cb4a21d33a7c3624158183beee191825bb77e
Masoud <masoud_mdar@yahoo.fr>
2024-12-29 15:07:21 +0100

    jsx version

---
+++ client/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import "./styles/navbar.css"
+import "./styles/mainPart.css"
+import "./styles/bookInfo.css"
+import "./styles/comments.css"
+import "./styles/styles.css"
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(<App />)
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://personal-library-msd.herokuapp.com"
+// export const BASE_URL = "https://personal-library-msd.herokuapp.com"
+export const BASE_URL = "http://localhost:5000"
 export const user = "demoUser"
